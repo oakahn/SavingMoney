ad6f886dbf6d8ace1e66de83950756e5fb045b2c
ChayawatS <mjpoak@gmail.com>
2018-06-04 09:47:11 +0700

    Create HistoryRouter, Create TransferRouter

---
+++ SavingMoney/Screen/History/Router/HistoryRouter.swift
@@ -0,0 +1,18 @@
+//
+//  HistoryRouter.swift
+//  SavingMoney
+//
+//  Created by chayarak on 4/6/2561 BE.
+//  Copyright © 2561 chayarak. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol HistoryRouterProtocol {
+    
+}
+
+class HistoryRouter: BaseRouter, HistoryRouterProtocol {
+    
+}
---
+++ SavingMoney/Screen/Transfer/Router/TransferRouter.swift
@@ -0,0 +1,18 @@
+//
+//  TransferRouter.swift
+//  SavingMoney
+//
+//  Created by chayarak on 4/6/2561 BE.
+//  Copyright © 2561 chayarak. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol TransferRouterProtocol {
+    
+}
+
+class TransferRouter: BaseRouter, TransferRouterProtocol {
+    
+}

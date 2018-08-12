2f469df919ed157801b2e1cc6a79115765f076f5
ChayawatS <mjpoak@gmail.com>
2018-08-12 19:27:40 +0700

    Goto firebase Success

--- SavingMoney/Screen/CreateTransfer/Presenter/CreateTransferPresenter.swift
+++ SavingMoney/Screen/CreateTransfer/Presenter/CreateTransferPresenter.swift
@@ -1,4 +1,6 @@
 import UIKit
+import Firebase
+import FirebaseDatabase
 
 protocol CreateTransferPresenterProtocol {
     func submitTransfer(title: String, amount: String, type: String)
@@ -7,14 +9,24 @@ protocol CreateTransferPresenterProtocol {
 class CreateTransferPersenter {
 
     var view: CreateTransferVCProtocol?
+    var dbReference: DatabaseReference?
     
     init(_ view: CreateTransferVCProtocol) {
         self.view = view
+        dbReference = Database.database().reference()
     }
 }
 
 extension CreateTransferPersenter: CreateTransferPresenterProtocol {
     func submitTransfer(title: String, amount: String, type: String) {
-        
+        let childListItem = ["CaptionAmerica", "Ironman", "Hulk", "TheFlash",
+                             "Thor", "Spiderman1"]
+        for item in childListItem{
+            guard let index = childListItem.index(of: item) else {
+                return
+            }
+            let listItem = String(index)
+            dbReference?.child("Human2").child(listItem).setValue(item)
+        }
     }
 }

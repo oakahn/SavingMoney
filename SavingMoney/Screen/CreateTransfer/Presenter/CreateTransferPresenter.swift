61e0332ce1705201dd1044c85a903e09a00cb87b
ChayawatS <mjpoak@gmail.com>
2018-11-12 16:55:40 +0700

    Success Create transfer

--- SavingMoney/Screen/CreateTransfer/Presenter/CreateTransferPresenter.swift
+++ SavingMoney/Screen/CreateTransfer/Presenter/CreateTransferPresenter.swift
@@ -31,41 +31,53 @@ extension CreateTransferPersenter: CreateTransferPresenterProtocol {
             if (dic == nil) {
                 let data: [String] = [self.createInsert(createModel)]
                 self.dbReference?.child(dateKeys).setValue(data)
-                self.updateValueFirebase(createModel.catagory)
+                self.updateValue(createModel)
                 return
             }
             guard var dictionary = snapshot.value as? [String] else { return }
             dictionary.append(self.createInsert(createModel))
             self.dbReference?.child(dateKeys).setValue(dictionary)
-            self.updateValueFirebase(createModel.catagory)
+            self.updateValue(createModel)
         })
     }
 }
 
 extension CreateTransferPersenter {
     
-    func createInsert(_ input: CreateModel) -> String {
+    private func createInsert(_ input: CreateModel) -> String {
         return input.type + "|" + input.catagory + "|" + input.amount + "|" + input.desc
     }
     
-    func submitSuccess() {
-        view?.redirectToSlipVC()
+    private func updateValue(_ createModel: CreateModel) {
+        updateCatagory(createModel: createModel)
+        updateType(createModel: createModel)
     }
     
-    func updateValueFirebase(_ child: String) {
-//        for i in ListTransfer().keyChild {
-//            updateValue(child: i)
-//        }
-        updateValue(child: child)
-//        submitSuccess()
-    }
-    
-    func updateValue(child: String) {
-        dbReference?.child(child).observeSingleEvent(of: .value, with: { (snapshot) in
+    private func updateType(createModel: CreateModel) {
+        dbReference?.child(createModel.catagory).observeSingleEvent(of: .value, with: { (snapshot) in
             guard let valueChild = snapshot.value as? String else { return }
             guard let valueInt = Int(valueChild) else { return }
-            self.dbReference?.child(child).setValue(String(valueInt + 1))
+            self.dbReference?.child(createModel.catagory).setValue(String(valueInt + 1))
+        })
+    }
+    
+    private func updateCatagory(createModel: CreateModel) {
+        let type = checkType(createModel: createModel)
+        dbReference?.child(type).observeSingleEvent(of: .value, with: { (snapshot) in
+            guard let valueChild = snapshot.value as? String else { return }
+            guard let valueInt = Int(valueChild) else { return }
+            guard let amount = Int(createModel.amount) else { return }
+            self.dbReference?.child(type).setValue(String(valueInt + amount))
             self.submitSuccess()
         })
+    }
+    
+    private func checkType(createModel: CreateModel) -> String {
+        if createModel.type == "in" { return "receiveTotal" }
+        return "payTotal"
+    }
+    
+    private func submitSuccess() {
+        view?.redirectToSlipVC()
     }
 }
